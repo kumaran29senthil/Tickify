0aed2ab251e2d20fcdb4dfc8075ad44b8f17ea4c
Kummaran <kumaransenthilarasu@gmail.com>
2025-04-10 11:05:40 +0530

    Initial commit from local project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Welcome to Ticket Marketplace</h1>
+    </div>
+  );
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://superb-aphid-23.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
