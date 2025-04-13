a7cb963d0c7db13e3ef567e10a8f2937a862b52b
Kumaran <150506302+kumaran29senthil@users.noreply.github.com>
2025-04-13 21:54:48 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import Header from "@/components/Header";
-
+import SyncUserWithConvex from "@/components/SyncUserWithConvex"; // adjust the path as needed
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
